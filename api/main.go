dcf92d9d4c97b933a3b2ba4549e1a8c2cccdda52
Bryce Chudomelka <bryce@inspiren.com>
2023-09-14 23:15:13 -0400

    Updated api/main.go

--- api/main.go
+++ api/main.go
@@ -1,1 +1,99 @@
+package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func protect(c *fiber.Ctx) error {
+	// Dummy auth check; replace with real logic.
+	token := c.Get("Authorization")
+	if token == "valid_token" {
+		return c.Next()
+	}
+	return c.Status(401).SendString("Unauthorized")
+}
+
+func makePayment(c *fiber.Ctx) error {
+	amount := c.FormValue("amount")
+	recipient := c.FormValue("recipient")
+	// Logic to make payment on XRP Ledger or any other financial service.
+	// Return success or failure.
+	return c.JSON(fiber.Map{"status": "success", "amount": amount, "recipient": recipient})
+}
+
+func getBalance(c *fiber.Ctx) error {
+	// Logic to fetch balance from the financial service
+	balance := "1000" // Dummy balance
+	return c.JSON(fiber.Map{"balance": balance})
+}
+
+func main() {
+	app := fiber.New()
+
+	// Firestore initialization
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("path/to/credentials.json") // Replace with your credentials file
+	client, err := firestore.NewClient(ctx, "your_project_id", opt)
+	if err != nil {
+		log.Fatalf("Firestore client creation failed: %v", err)
+	}
+
+	// Signal Protocol initialization
+	signalProtocol := &SignalProtocol{}
+	if err := signalProtocol.GenerateKeys(); err != nil {
+		log.Fatal("Failed to generate keys:", err)
+	}
+
+	app.Use("/ws", protect)
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		// WebSocket handling logic
+		address := signal.NewSignalAddress("+11111111111", 1) // Replace with real address
+
+		for {
+			mt, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				break
+			}
+
+			// Decrypt incoming message
+			decryptedMsg, err := signalProtocol.DecryptMessage(msg, address)
+			if err != nil {
+				log.Println("Decryption failed:", err)
+				break
+			}
+
+			// Do something with the decrypted message, e.g., save it to Firestore
+			_, _, err = client.Collection("messages").Add(ctx, map[string]interface{}{
+				"message": decryptedMsg,
+			})
+			if err != nil {
+				log.Fatalf("Failed adding message: %v", err)
+			}
+
+			// Encrypt outgoing message
+			reply := []byte("Server reply")
+			encryptedReply, err := signalProtocol.EncryptMessage(reply, address)
+			if err != nil {
+				log.Println("Encryption failed:", err)
+				break
+			}
+
+			if err := c.WriteMessage(mt, encryptedReply); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+	}))
+
+	// Routes for financial transactions
+	xrpl := app.Group("/xrpl", protect)
+	xrpl.Post("/payment", makePayment)
+	xrpl.Get("/balance", getBalance)
+
+	app.Listen(":8080")
+}
